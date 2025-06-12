ed6a32bc0d8a7a744b2ba28c38d8dc8267732862
Jeroen Dekkers <jeroen@dekkers.ch>
2025-06-12 16:17:59 +0200

    Make POSTGRES_SSL_ROOTCERT optional and set POSTGRES_SSL_MODE by default to "required"

--- rocky/rocky/settings.py
+++ rocky/rocky/settings.py
@@ -245,10 +245,7 @@ except ImproperlyConfigured:
 DATABASES = {"default": POSTGRES_DB}
 
 if env.bool("POSTGRES_SSL_ENABLED", False):
-    DATABASES["default"]["OPTIONS"] = {
-        "sslmode": env("POSTGRES_SSL_MODE"),
-        "sslrootcert": env.path("POSTGRES_SSL_ROOTCERT"),
-    }
+    DATABASES["default"]["OPTIONS"] = {"sslmode": env("POSTGRES_SSL_MODE", "required")}
 
     POSTGRES_SSL_CERT = env.path("POSTGRES_SSL_CERT", default="")
     POSTGRES_SSL_KEY = env.path("POSTGRES_SSL_KEY", default="")
@@ -256,6 +253,11 @@ if env.bool("POSTGRES_SSL_ENABLED", False):
     if POSTGRES_SSL_CERT and POSTGRES_SSL_KEY:
         DATABASES["default"]["OPTIONS"]["sslcert"] = POSTGRES_SSL_CERT
         DATABASES["default"]["OPTIONS"]["sslkey"] = POSTGRES_SSL_KEY
+
+    POSTGRES_SSL_ROOTCERT = env.path("POSTGRES_SSL_ROOTCERT", default="")
+    if POSTGRES_SSL_ROOTCERT:
+        DATABASES["default"]["OPTIONS"]["sslrootcert"] = POSTGRES_SSL_ROOTCERT
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
